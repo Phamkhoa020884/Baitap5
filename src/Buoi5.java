00962dab664e5cc8e3d9b8700d09f76a5be7aea3
khoa <hatgiong@gmail.com>
2021-10-28 18:23:06 +0700

    sưa bai

--- src/Buoi5.java
+++ src/Buoi5.java
@@ -1,27 +1,27 @@
 public class Buoi5 {
     public static void main(String[] args) {
-//        int total=100000;
-//        String mess="";
-//        if (total>500000){
-//            total =( int )(total*0.8);
-//            mess="Giảm 20%";
-//
-//        }else if (total >200000){
-//
-//            total =( int )(total*0.85);
-//            mess="Giảm 15%";
-//        }else if (total>100000){
-//
-//            total =( int )(total*0.9);
-//            mess="Giảm 10%";
-//        }else {
-//            mess="Không giảm";
-//
-//        }
-//        System.out.println(mess);
-//        System.out.println("total"+ total);
+        int total=100000;
+        String mess="";
+        if (total>500000){
+            total =( int )(total*0.8);
+            mess="Giảm 20%";
+
+        }else if (total >200000){
+
+            total =( int )(total*0.85);
+            mess="Giảm 15%";
+        }else if (total>100000){
+
+            total =( int )(total*0.9);
+            mess="Giảm 10%";
+        }else {
+            mess="Không giảm";
+
+        }
+        System.out.println(mess);
+        System.out.println("total"+ total);
 
 
-        
+
     }
 }
